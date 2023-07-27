efd322beade1219284d895db5ab4099ab63d6e51
mefdev <mohamedmeftah2018@gmail.com>
2023-07-27 17:27:54 +0100

    Remove duplicates

---
+++ 6kyu/countDuplicates.js
@@ -0,0 +1,21 @@
+function duplicateCount(text){
+    const lowerText = text.toLowerCase()
+    let obj = {}
+    let count = 0;
+    lowerText.split("").reduce((currentVal) => {
+        if (!obj[currentVal])
+          {
+            obj[currentVal] = 1
+          }
+       else obj[currentVal]++
+    }, 0)
+   for (let i in obj)
+     {
+       if (obj[i] > 1)
+         {
+           count++
+         }    
+     }
+   return count;
+ }
+            
